fd9908ed59a578ad258984722ecb21aea4065734
Sougata Ghosh <155520429+SougataGh@users.noreply.github.com>
2024-02-04 00:32:13 +0530

    Update Wallet.hpp

--- Wallet.hpp
+++ Wallet.hpp
@@ -8,7 +8,7 @@ public:
     Wallet():balance(0){}
     virtual void add_money(const double &amount);
     virtual void extract_money(const double &amount);
-    virtual double get_balance() const;
+    inline virtual double get_balance() const;
 };
 
 #endif // WALLET_H_INCLUDED
