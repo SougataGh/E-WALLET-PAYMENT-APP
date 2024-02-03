4eb181e93de219ee0d9bdf5d0170bc7682136e07
Sougata Ghosh <155520429+SougataGh@users.noreply.github.com>
2024-02-04 00:18:58 +0530

    Create Transaction.hpp

---
+++ Transaction.hpp
@@ -0,0 +1,17 @@
+#ifndef TRANSACTION_H_INCLUDED
+#define TRANSACTION_H_INCLUDED
+
+#include<iostream>
+class Transaction
+{
+protected:
+
+    double amount;
+    std::string info;
+public:
+    Transaction():amount(0),info(""){}
+     void set_transaction(const double &,const std::string &);
+     void show_transaction() const;
+};
+
+#endif // TRANSACTION_H_INCLUDED
