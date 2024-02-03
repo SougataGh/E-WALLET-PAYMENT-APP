6ab429c50d0f88131f52176c0812952f406d2948
Sougata Ghosh <155520429+SougataGh@users.noreply.github.com>
2024-02-04 00:20:24 +0530

    Create User.hpp

---
+++ User.hpp
@@ -0,0 +1,24 @@
+#ifndef USER_H_INCLUDED
+#define USER_H_INCLUDED
+
+#include<iostream>
+
+#include"Wallet.h"
+#include"Transaction_History.h"
+
+class User : public Wallet,public Transaction_History
+{
+protected:
+
+    std::string user_name;
+    std::string ph_no;
+    std::string passcode;
+public:
+    virtual void set_user(const std::string &,const std::string &,const std::string &);
+    virtual std::string get_user_name() const;
+    virtual std::string get_ph_no() const;
+    virtual std::string get_passcode() const;
+    friend std::ostream& operator<<(std::ostream&,User&);
+};
+
+#endif // USER_H_INCLUDED
