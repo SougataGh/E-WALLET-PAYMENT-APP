64ad8d7d089cf9a5d284850c618db73e52c27a6b
Sougata Ghosh <155520429+SougataGh@users.noreply.github.com>
2024-02-04 00:25:45 +0530

    Create Wallet.cpp

---
+++ Wallet.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+#include "Wallet.h"
+
+void Wallet::add_money(const double &amount)
+{
+    if(amount<0)
+    {
+        std::cout<<"INVALID AMOUNT....FAILED TO CREDIT THE ACCOUNT!.....\n\n";
+        return;
+    }
+    balance+=amount;
+}
+void Wallet::extract_money(const double &amount)
+{
+    if(amount<0)
+    {
+        std::cout<<"INVALID AMOUNT....FAILED TO DEDIT THE ACCOUNT!.....\n\n";
+        return;
+    }
+    balance+=amount;
+}
+double Wallet::get_balance() const
+{
+    return balance;
+}
