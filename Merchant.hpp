c09cdc9fd8799043e663696efe6dc65052627df9
Sougata Ghosh <155520429+SougataGh@users.noreply.github.com>
2024-02-04 00:17:43 +0530

    Create Merchant.hpp

---
+++ Merchant.hpp
@@ -0,0 +1,16 @@
+#ifndef MERCHANT_H_INCLUDED
+#define MERCHANT_H_INCLUDED
+
+#include<iostream>
+#include<string>
+
+#include"User.h"
+#include"Reward_Cashback.h"
+
+class Merchant : public User,public Reward_Cashback
+{
+public:
+    void set_user(const std::string&,const std::string&,const std::string&);
+    double get_criteria_amount() const;
+};
+#endif // MERCHANT_H_INCLUDED
